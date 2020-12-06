4b23267bd7cc68f6f7a7e42766f726ad4e9cfd15
stepin104311 <69067336+stepin104311@users.noreply.github.com>
2020-12-06 17:11:45 +0530

    Update test_sdlc.c

--- test/test_sdlc.c
+++ test/test_sdlc.c
@@ -1,43 +1,75 @@
-#include "unity.h"
-#include "sdlc.h"
-/* Required by the unity test framework */
-void setUp(){}
-/* Required by the unity test framework */
-void tearDown(){}
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 
-void test_squareroot(void)
-{
-  TEST_ASSERT_EQUAL(5, squareroot(25));
-  TEST_ASSERT_EQUAL(10, squareroot(100));
-}
-void test_reciprocal(void)
-{
-  TEST_ASSERT_EQUAL(0.1000, reciprocal(10));
-  TEST_ASSERT_EQUAL(0.04, reciprocal(25));
+/* Modify these two lines according to the project */
+#include <sdlc.h>
+#define PROJECT_NAME    "Sdlc"
+
+/* Prototypes for all the test functions */
+void test_squareroot(void);
+void test_reciprocal(void);
+void test_power(void);
+void test_exponential(void);
+
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "squareroot", test_squareroot);
+  CU_add_test(suite, "reciprocal", test_reciprocal);
+  CU_add_test(suite, "power", test_power);
+  CU_add_test(suite, "exponential", test_exponential);
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
 }
 
-void test_power(void)
-{
-  TEST_ASSERT_EQUAL(8, power(2, 3));
-  TEST_ASSERT_EQUAL(100, power(10, 2));
-}
-void test_exponential(void)
-{
-  TEST_ASSERT_EQUAL(7.3890, exponential(2));
-  TEST_ASSERT_EQUAL(2980.9579, exponential(8));
+/* Write all the test functions */ 
+
+void test_squareroot(void) {
+  CU_ASSERT(10 == squareroot(100));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == squareroot(25));
 }
 
-int test_main(void)
-{
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
 
-/* Run Test functions */
-  RUN_TEST(test_squareroot);
-  RUN_TEST(test_reciprocal);
-  RUN_TEST(test_power);
-  RUN_TEST(test_exponential);
-  /* Close the Unity Test Framework */
-  return UNITY_END();
+void test_inverse(void) {
+  CU_ASSERT(0.1== reciprocal(10));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2==reciprocal(3));
+}
+
+
+void test_power(void) {
+  CU_ASSERT(100 == power(10, 2));
+  
+  /* Dummy fail*/
+  CU_ASSERT(6 == power(3, 3));
+}
+
+
+void test_exponential(void) {
+  CU_ASSERT(7.3890== exponential(2));
+  
+  /* Dummy fail*/
+  CU_ASSERT(100==exponential(3));
 }
 
